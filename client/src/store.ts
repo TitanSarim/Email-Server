bea5ce1f4ed04e388d96e2c89f47b600440a807c
TitanSarim <muhammadsarim203@gmail.com>
2024-05-31 19:51:11 +0500

    redux config

---
+++ client/src/store.ts
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers } from "redux"
+import {thunk} from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const reducer = combineReducers({
+
+})
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+
+    reducer: persistedReducer,
+
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: false, // Ignore serializability for now
+    }).concat(thunk),
+
+    devTools: process.env.NODE_ENV !== 'production',
+
+
+})
+
+export const persistor = persistStore(store);
+export default store;
